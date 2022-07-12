b1811435d19318beabc138d15543005c4271df13
Jerry Duijm <jduijm@kaliber.net>
2022-07-12 16:40:18 +0200

    Add config and translation

--- src/DocumentActionProductionReview.js
+++ src/DocumentActionProductionReview.js
@@ -1,12 +1,16 @@
 import { EarthGlobeIcon } from '@sanity/icons'
 import { resolveProductionUrl } from 'part:@kaliber/resolve-production-url'
+import pluginConfig from 'config:@kaliber/sanity-plugin-preview'
+import { i18n as getI18n } from './i18n'
 
 export function DocumentActionProductionReview({ published }) {
   if (!published) return null
 
+  const i18n = getI18n(pluginConfig.language)
+
   return {
     icon: EarthGlobeIcon,
-    label: 'View live',
+    label: i18n['view-live'],
     color: 'success',
     onHandle: () => {
       resolveProductionUrl(published).then(url => { window.open(url, '_blank').focus() })
